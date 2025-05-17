db8c48e2cdd6f35b320a99d8cb42f457962e8fb8
Timothy Nguyen <timothy.nguyen.9@icloud.com>
2025-05-16 22:29:20 -0500

    simple stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-
-  /* We don't want vite to serve static files in development.*/
   publicDir: false,
-  /* if so, it'll copy the files from `./public` to `oudDir` (below) */
 
   build: {
-    lib: {
-      entry: 'browser/main.ts',
-      name: 'BrowserBundle',
-      fileName: () => 'bundle.js',
-      formats: ['es'],
-    },
     outDir: 'public/js',
     emptyOutDir: false,
+    sourcemap: true,
+    minify: 'esbuild',
     watch: {},
+
+    rollupOptions: {
+      input: 'browser/main.ts',
+      output: {
+        entryFileNames: 'bundle.js',
+      }
+    }
   }
 });
