2067fe3a36341f03db97f304c67ffdfd960f2c29
Timothy Nguyen <timothy.nguyen.9@icloud.com>
2025-05-16 21:43:27 -0500

    telling my future self

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+
+  /* We don't want vite to serve static files in development.*/
   publicDir: false,
+  /* if so, it'll copy the files from `./public` to `oudDir` (below) */
+
   build: {
     lib: {
       entry: 'browser/main.ts',
